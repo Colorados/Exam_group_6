bb59fc8b573398a7ab74ab301f9ffe349c253bfa
Seitek <fall3nleaf@gmail.com>
2020-08-01 15:48:13 +0600

    Добавлено поле Статус

--- source/guest_book_app/models.py
+++ source/guest_book_app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+STATUSES = {
+    'active': 'Активно',
+    'blocked': 'Заблокировано'
+}
 class GuestBook(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Имя автора записи')
     inbox = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Адрес электронной почты')
     record = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Запись')
+    status = models.CharField(max_length=20, choices=STATUSES, default='active', verbose_name='Статус')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
