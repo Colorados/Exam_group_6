5e8b1569801b217c7bfa6065fe2a8858890a685d
Seitek <fall3nleaf@gmail.com>
2020-08-01 17:23:59 +0600

    Добавлена модель сортировки и поиска по имени

--- source/guest_book_app/models.py
+++ source/guest_book_app/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib import admin
+
 
 STATUSES = [
     ('active', 'Активно'),
     ('blocked', 'Заблокировано')
 ]
+
+
 class GuestBook(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Имя автора записи')
     inbox = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Адрес электронной почты')
@@ -18,3 +22,11 @@ class GuestBook(models.Model):
     class Meta:
         verbose_name = 'Запись'
         verbose_name_plural = 'Записи'
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'record', 'inbox', 'created_at']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['name', 'record', 'inbox', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
